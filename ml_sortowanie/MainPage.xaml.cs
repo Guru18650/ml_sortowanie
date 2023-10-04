ecc6ce9b2e9d010abe3c3c07d1b0b5398a74f404
Guru18650 <guruwszystkiego@gmail.com>
2023-10-04 18:22:19 +0200

    init

--- ml_sortowanie/MainPage.xaml.cs
+++ ml_sortowanie/MainPage.xaml.cs
@@ -2,23 +2,89 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        List<string> data = new List<string>();
+        string chars = "qwertyuioplkjhgfdsazxcvbnm";
+        static Random rnd = new Random();
+        List<String> unsort = new List<string>();
 
         public MainPage()
         {
             InitializeComponent();
+            tryb.SelectedIndex = 0;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+       
+
+        private void generuj_Clicked(object sender, EventArgs e)
         {
-            count++;
+            if (String.IsNullOrEmpty(ilosc.Text))
+            {
+                DisplayAlert("Błąd", "Wpisz wszystkie dane", "OK");
+                return;
+            }
+            data.Clear();
+            if(lCheck.IsChecked) {
+                for (int i = 0; i < int.Parse(ilosc.Text); i++)
+                {
+                    data.Add(rnd.Next(0,9).ToString());
+                }
+            } else
+            {
+                for (int i = 0; i < int.Parse(ilosc.Text); i++)
+                {
+                    data.Add(chars[rnd.Next(0, chars.Length)].ToString());
+                }
+            }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            unsort = new List<string>(data);
+            if(tryb.SelectedIndex == 0)
+            { 
+                data.Sort();
+            } else if(tryb.SelectedIndex == 1)
+            {
+                data.Sort();
+                data.Reverse();
+            } else
+            {
+                data.Shuffle();
+            }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+
+
+            sort.IsEnabled = true;
+            nosort.IsEnabled = true;
+            stats.IsEnabled = true;
+
         }
+
+        private void sort_Clicked(object sender, EventArgs e)
+        {
+            var s = "";
+            foreach (var item in data)
+            {
+                s += item+", ";
+            }
+            DisplayAlert("Posortowane", s, "ok");
+        }
+
+        private void nosort_Clicked(object sender, EventArgs e)
+        {
+            var s = "";
+            foreach (var item in unsort)
+            {
+                s += item + ", ";
+            }
+            DisplayAlert("Nie posortowane", s, "ok");
+        }
+
+        private void stats_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+       
     }
+
+
 }
---
+++ ml_sortowanie/Statystyki.xaml.cs
@@ -0,0 +1,10 @@
+namespace ml_sortowanie;
+
+public partial class Statystyki : ContentPage
+{
+	public Statystyki()
+	{
+		InitializeComponent();
+		lista.ItemsSource 
+	}
+}
